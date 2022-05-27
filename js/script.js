a124b3e9040891a679d09aaa78d758ded62f1ea3
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-27 19:01:06 +0200

    modifiche all'aspetto grafico

--- js/script.js
+++ js/script.js
@@ -14,3 +14,9 @@ Milestone 4
 Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 */
 
+var app = new Vue({
+    el : '.container',
+    data : {
+        
+    }
+})
